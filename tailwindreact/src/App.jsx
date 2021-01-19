4040896f6739164ce7fdb0192b19cbc36cd0e1de
stefanysamaniego <kerllyayelen@gmail.com>
2021-01-18 21:01:32 -0500

    react

---
+++ tailwindreact/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Tailwind</h1>
+            <h2>css</h2>
+            <button className="btn btn-blue btn-blue:hover">botón</button> 
+        </div>
+        
+    )
+}
+
+export default App
---
+++ tailwindreact/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './assets/main.css';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
